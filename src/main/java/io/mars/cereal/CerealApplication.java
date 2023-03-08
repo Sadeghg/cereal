83f902599d42c3bc38d219121b86b4f05d95ebae
Sadeghg <Sghorogi@gmail.com>
2023-03-08 03:13:23 -0800

    Initial commit
    Initialized project as spring boot application with starter web and
    database maven dependencies
    
    Solves no issu

---
+++ src/main/java/io/mars/cereal/CerealApplication.java
@@ -0,0 +1,13 @@
+package io.mars.cereal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CerealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CerealApplication.class, args);
+    }
+
+}

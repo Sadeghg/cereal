c4e304c13bcf6a9f8c1bca9d487ae24a64a3bb0c
Sadeghg <Sghorogi@gmail.com>
2023-03-21 07:59:53 -0700

    feat(model): add product and card relations to card item entity
    add product and card relations to card item entity both as many to one relations
    with the card item entity owening the realtion
    
    resolves no issue

--- src/main/java/io/mars/cereal/model/CardItem.java
+++ src/main/java/io/mars/cereal/model/CardItem.java
@@ -12,4 +12,12 @@ public class CardItem {
             , initialValue = 1, allocationSize = 10)
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
+    @JoinColumn(name = "card_id", foreignKey = @ForeignKey(name = "card_fk_id"))
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Card card;
+
+    @JoinColumn(name = "product_id", foreignKey = @ForeignKey(name = "product_fk_id"))
+    @ManyToOne(fetch = FetchType.EAGER)
+    private Product product;
 }

6d370c290f1f5e500fb6a629626a44cee94faf0a
Sadeghg <Sghorogi@gmail.com>
2023-03-20 23:12:37 -0700

    feat(model): add extra details to product model
    add extra details to product model as element collection map
    
    resolves no issue yet

--- src/main/java/io/mars/cereal/model/Product.java
+++ src/main/java/io/mars/cereal/model/Product.java
@@ -3,6 +3,8 @@ package io.mars.cereal.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.Map;
+
 @Data
 @Entity
 public class Product {
@@ -11,10 +13,18 @@ public class Product {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name="product_name", columnDefinition = "TEXT")
+    @Column(name = "product_name", columnDefinition = "TEXT")
     private String name;
 
     @Column(name = "product_price", nullable = false)
     private Double price;
 
+    @ElementCollection
+    @CollectionTable(name = "product_details",
+            joinColumns = {@JoinColumn(
+                    name = "product_id",
+                    referencedColumnName = "id",
+                    foreignKey = @ForeignKey(name = "product_details_fk"))
+            })
+    private Map<String, String> details;
 }

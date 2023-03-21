42cd4637b0fc2eea2d5221407f0ac5e7d90069c6
Sadeghg <Sghorogi@gmail.com>
2023-03-20 22:15:04 -0700

    Initial commit

---
+++ src/test/java/io/mars/cereal/CerealApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mars.cereal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CerealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

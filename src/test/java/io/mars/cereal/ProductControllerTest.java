e9171b70fadce0241d3420ecbe63b3f037c69100
Sadeghg <Sghorogi@gmail.com>
2023-03-26 02:47:27 -0700

    test(contrller): mock create product in controller
    mock create method in test class

--- src/test/java/io/mars/cereal/ProductControllerTest.java
+++ src/test/java/io/mars/cereal/ProductControllerTest.java
@@ -4,6 +4,8 @@ import io.mars.cereal.controller.specification.ProductEndpoint;
 import io.mars.cereal.model.Company;
 import io.mars.cereal.model.Product;
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -20,8 +22,9 @@ public class ProductControllerTest {
     @Mock
     private ProductEndpoint endpoint;
 
-    @Test
-    public void findByIdSuccess(){
+    private final Product product = product();
+
+    public static Product product(){
         Company lg = new Company();
         lg.setName("LG");
         lg.setId(1L);
@@ -37,14 +40,20 @@ public class ProductControllerTest {
         tv.setCompany(lg);
         tv.setPrice(500D);
         tv.setId(2L);
+        return tv;
+    }
 
-        when(endpoint.findById(anyLong())).thenReturn(ResponseEntity.ok(tv));
+    @Test
+    public void findByIdSuccess(){
+
+
+        when(endpoint.findById(anyLong())).thenReturn(ResponseEntity.ok(product));
 
         assertTrue(endpoint.findById(anyLong()).hasBody());
-        assertEquals(tv.getId(), endpoint.findById(anyLong()).getBody().getId());
-        assertEquals(tv.getDetails().entrySet().size(),
+        assertEquals(product.getId(), endpoint.findById(anyLong()).getBody().getId());
+        assertEquals(product.getDetails().entrySet().size(),
                 endpoint.findById(anyLong()).getBody().getDetails().entrySet().size());
-        assertEquals(tv.getCompany().getName(),
+        assertEquals(product.getCompany().getName(),
                 endpoint.findById(anyLong()).getBody().getCompany().getName());
     }
 
@@ -52,6 +61,17 @@ public class ProductControllerTest {
     public void findByIdNotFound(){
         when(endpoint.findById(anyLong())).thenThrow(new RuntimeException("content not found"));
         assertThrows(RuntimeException.class, () -> endpoint.findById(anyLong()));
+    }
+
+    @Test
+    public void createProduct(){
+        when(endpoint.createProduct(anyString(), anyDouble(), anyString())).thenReturn(ResponseEntity.ok(product));
+        assertTrue(endpoint.createProduct(anyString(), anyDouble(), anyString()).hasBody());
+        assertEquals(product.getId(), endpoint.createProduct(anyString(), anyDouble(), anyString()).getBody().getId());
+        assertEquals(product.getDetails().entrySet().size(),
+                endpoint.createProduct(anyString(), anyDouble(), anyString()).getBody().getDetails().entrySet().size());
+        assertEquals(product.getCompany().getName(),
+                endpoint.createProduct(anyString(), anyDouble(), anyString()).getBody().getCompany().getName());
     }
 
 }

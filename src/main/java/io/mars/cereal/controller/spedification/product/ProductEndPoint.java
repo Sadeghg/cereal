c52d679050ab0da41054f4c90e17cc46e0f0ff4e
Sadeghg <Sghorogi@gmail.com>
2023-03-26 00:32:11 -0700

    Revert "feat(controller): add prodcut endpoint specification"
    This reverts commit 08378eed901f16e620e936b86e82c2063072390f.

--- src/main/java/io/mars/cereal/controller/spedification/product/ProductEndPoint.java
+++
@@ -1,8 +0,0 @@
-package io.mars.cereal.controller.spedification.product;
-
-import io.mars.cereal.model.Product;
-import org.springframework.http.ResponseEntity;
-
-public class ProductEndPoint {
-
-}

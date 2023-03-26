9b4590bb148431208727274c217dab331e4bae99
Sadeghg <Sghorogi@gmail.com>
2023-03-26 00:41:28 -0700

    feat(controller): add find product by id
    add the method to specification endpoint and the contrller which impelemnts it,
    the method returns response entity of product

--- src/main/java/io/mars/cereal/controller/ProductController.java
+++ src/main/java/io/mars/cereal/controller/ProductController.java
@@ -1,9 +1,17 @@
 package io.mars.cereal.controller;
 
+import io.mars.cereal.controller.specification.ProductEndpoint;
+import io.mars.cereal.model.Product;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/product")
-public class ProductController {
+public class ProductController implements ProductEndpoint {
+
+    @Override
+    public ResponseEntity<Product> findById(Long id) {
+        return null;
+    }
 }

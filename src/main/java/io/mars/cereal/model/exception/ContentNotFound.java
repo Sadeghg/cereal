e923df58650e3014674869f7edb94baf78237859
Sadeghg <Sghorogi@gmail.com>
2023-03-30 01:58:07 -0700

    model(exception): add content not found exception
    exception for situations when there is no entity for given id

---
+++ src/main/java/io/mars/cereal/model/exception/ContentNotFound.java
@@ -0,0 +1,15 @@
+package io.mars.cereal.model.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public class ContentNotFound extends RuntimeException {
+
+    private final String message;
+    private HttpStatus httpStatus = HttpStatus.NOT_FOUND;
+
+}

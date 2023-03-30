6d49737b3aae9af7e7860048203f96bacb8926aa
Sadeghg <Sghorogi@gmail.com>
2023-03-29 21:47:28 -0700

    refactor(infrastructure): edit controller skeleton

--- src/main/java/io/mars/cereal/controller/ProductController.java
+++ src/main/java/io/mars/cereal/controller/ProductController.java
@@ -1,6 +1,5 @@
 package io.mars.cereal.controller;
 
-import io.mars.cereal.controller.specification.ProductEndpoint;
 import io.mars.cereal.model.Product;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,24 +7,20 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/product")
-public class ProductController implements ProductEndpoint {
+public class ProductController {
 
-    @Override
     public ResponseEntity<Product> findById(Long id) {
         return null;
     }
 
-    @Override
     public ResponseEntity<Product> createProduct(String name, Double price, String companyName) {
         return null;
     }
 
-    @Override
     public ResponseEntity<Product> updateProduct(Long id, String name, Double price, String companyName) {
         return null;
     }
 
-    @Override
     public void deleteProduct(Long id) {
 
     }

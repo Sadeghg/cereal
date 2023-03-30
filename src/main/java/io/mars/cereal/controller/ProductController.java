8cd50386a843e7a26ff72b6464767cec28e24a42
Sadeghg <Sghorogi@gmail.com>
2023-03-30 05:27:55 -0700

    controller(product): add url mapping for find by id

--- src/main/java/io/mars/cereal/controller/ProductController.java
+++ src/main/java/io/mars/cereal/controller/ProductController.java
@@ -2,14 +2,17 @@ package io.mars.cereal.controller;
 
 import io.mars.cereal.model.Product;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/product")
+@RequestMapping("/api/product/")
 public class ProductController {
 
-    public ResponseEntity<Product> findById(Long id) {
+    @GetMapping("{id}")
+    public ResponseEntity<Product> findById(@PathVariable Long id) {
         return null;
     }
 

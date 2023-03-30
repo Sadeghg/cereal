7c300948e8a3b01300b87295f31a3d444044fa69
Sadeghg <37359428+Sadeghg@users.noreply.github.com>
2023-03-30 16:06:17 +0330

    annotate product controller methods (#37)
    * controller(product): add url mapping for find by id
    
    * controller(product) add url mapping for create product
    
    * controller(product): add url mapping for update product
    
    * controller(product): add url mapping for delete product
    
    * controller(product): correct url mapping for update product
    
    * controller(product): correct url mapping for save product

--- src/main/java/io/mars/cereal/controller/ProductController.java
+++ src/main/java/io/mars/cereal/controller/ProductController.java
@@ -2,26 +2,29 @@ package io.mars.cereal.controller;
 
 import io.mars.cereal.model.Product;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/product")
+@RequestMapping("/api/product/")
 public class ProductController {
 
-    public ResponseEntity<Product> findById(Long id) {
+    @GetMapping("{id}")
+    public ResponseEntity<Product> findById(@PathVariable Long id) {
         return null;
     }
 
-    public ResponseEntity<Product> createProduct(String name, Double price, String companyName) {
+    @PostMapping("save")
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
         return null;
     }
 
-    public ResponseEntity<Product> updateProduct(Long id, String name, Double price, String companyName) {
+    @PutMapping("update")
+    public ResponseEntity<Product> updateProduct(@RequestBody Product product) {
         return null;
     }
 
-    public void deleteProduct(Long id) {
+    @DeleteMapping("delete/{id}")
+    public void deleteProduct(@PathVariable  Long id) {
 
     }
 }

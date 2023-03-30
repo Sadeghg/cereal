5b97b320593ec4ecc07cb51760c6255664e90611
Sadeghg <Sghorogi@gmail.com>
2023-03-30 01:09:11 -0700

    service(product): add service test class

---
+++ src/test/java/io/mars/cereal/ProductServiceTest.java
@@ -0,0 +1,11 @@
+package io.mars.cereal;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductServiceTest {
+
+
+
+}

7e491ae7c1dca9b0ad5026cdf2d68b9e5faf8b88
Sadeghg <Sghorogi@gmail.com>
2023-03-30 01:41:58 -0700

    service(interface): add product service class

---
+++ src/main/java/io/mars/cereal/service/product/ProductService.java
@@ -0,0 +1,7 @@
+package io.mars.cereal.service.product;
+
+import io.mars.cereal.model.Product;
+import io.mars.cereal.service.generic.GenericService;
+
+public interface ProductService extends GenericService<Product, Long> {
+}

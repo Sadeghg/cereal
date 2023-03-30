71ce93f189fb798fff7f4743fbbd22dd74fb1650
Sadeghg <Sghorogi@gmail.com>
2023-03-30 01:48:54 -0700

    service(product): add mock and inject mock
    add service and mock repository to test class

--- src/test/java/io/mars/cereal/ProductServiceTest.java
+++ src/test/java/io/mars/cereal/ProductServiceTest.java
@@ -1,9 +1,21 @@
 package io.mars.cereal;
 
+import io.mars.cereal.data.product.ProductRepository;
+import io.mars.cereal.service.product.ProductService;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
+
+    @Mock
+    private ProductRepository repository;
+
+    @InjectMocks
+    private ProductService productService;
+
+
 
 }

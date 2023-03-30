64b29f67574227712f6c06101e80c48c0392c93f
Sadeghg <Sghorogi@gmail.com>
2023-03-30 02:13:37 -0700

    service(interface): add save all method
    with collection as input

--- src/main/java/io/mars/cereal/service/generic/GenericService.java
+++ src/main/java/io/mars/cereal/service/generic/GenericService.java
@@ -1,7 +1,10 @@
 package io.mars.cereal.service.generic;
 
+import java.util.Collection;
+
 public interface GenericService <T, ID>{
 
     T save(T t);
 
+    Collection<T> saveAll(Collection<T> t);
 }

808012e2e73aa43f128c03d78e91575000e1ca5c
Sadeghg <Sghorogi@gmail.com>
2023-03-29 22:29:21 -0700

    infrastructure(product): add product repository test class

---
+++ src/test/java/io/mars/cereal/ProductRepositoryTest.java
@@ -0,0 +1,10 @@
+package io.mars.cereal;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductRepositoryTest {
+
+
+}
